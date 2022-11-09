0ed5f762603bf2c5572e628bb34aa0dd312fc524
brunotg02 <brunotg02@gmail.com>
2022-11-09 14:48:24 -0300

    progress on pool_mk2

--- src/pools/Pool_mk2.java
+++ src/pools/Pool_mk2.java
@@ -15,6 +15,7 @@ public class Pool_mk2
 		private Log logger;
 
 		private Boolean flag_PrimeiraVez_movimentar_p=true;
+		private Boolean flag_PrimeiraVez_adicionar=true;
 
 /*	
 		
@@ -22,10 +23,34 @@ public class Pool_mk2
 	
 	public Pool_mk2(int tamanho_da_pool, int quantidade_portas, Log logger, Pessoa_Thread[] pessoas) 
 	{
+		if(pessoas==null)		{throw new java.lang.Error("Pool =>\nO valor do parametro Pessoa_Thread e nulo");}
+		if(pessoas.length==0)	{throw new java.lang.Error("Pool =>\nNao existem pessoas para adicionar a thread");}
+	
+	
 		this.pool=new Pessoa_Thread[tamanho_da_pool];
 		this.logger=logger;
-	}
 	
+		
+		for(int i=0; i!=pessoas.length;i++)
+		{
+			pool_adicionar_pessoa(pessoas[i]);
+		}
+		
+		logger.receber("A pool foi instancializada", null);
+	}
+	private void pool_adicionar_pessoa(Pessoa_Thread p) 
+	{
+		if(!flag_PrimeiraVez_adicionar) {return;}
+		int indice = pool_posicoes_ocupadas.size();
+		
+		pool[indice]=p;
+		pool_posicoes_ocupadas.add(Integer.valueOf(indice));
+		
+		flag_PrimeiraVez_adicionar=false;
+	}
+
+
+
 	public void pool_iniciarThreadPessoa() 
 	{
 		for(int i=0; i!=pool.length; i++)
@@ -34,14 +59,6 @@ public class Pool_mk2
 		}
 	}
 	
-	private void pool_adicionar_pessoa(Pessoa_Thread p) 
-	{
-		if(!flag_PrimeiraVez_movimentar_p) {return;}
-		int indice = pool_posicoes_ocupadas.size();
-		
-		pool[indice]=p;
-		pool_posicoes_ocupadas.add(Integer.valueOf(indice));
-	}
 	
 	public void pool_movimentar_pessoas(int tempo_milisegundos) 
 	{if(!flag_PrimeiraVez_movimentar_p) {return;}
