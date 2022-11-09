b5b1d408ccef2d0b0e7c08746ec6e0cd056761a6
brunotg02 <brunotg02@gmail.com>
2022-11-09 12:22:48 -0300

    -

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,20 +1,12 @@
 package app;
-
+import interfaces.*; import models.*; import pools.*; import threads.*;
 import java.util.ArrayList;
 
-public class Application {
+public class Application 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ArrayList<Integer> iii = new ArrayList<Integer> ();
-	
-		iii.add(112331);
-		iii.add(212331);
-		iii.add(312331);
-		iii.add(412331);
-	
-		iii.remove(Integer.valueOf(112331));
-		System.out.println(iii.get(0));
+	public static void main(String[] args) 
+	{
 		
 	}
 
