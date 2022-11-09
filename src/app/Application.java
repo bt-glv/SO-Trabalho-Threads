055d3e83e36a1f076c457909b6903fc675f9133b
Marco Aurélio De Faria <marcoaureliodefariar@gmail.com>
2022-11-09 14:09:52 -0300

    começo

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,17 +1,25 @@
 package app;
-import interfaces.*; import models.*; import pools.*; import threads.*;
-import java.util.ArrayList;
+import java.util.Scanner;
+
+import pools.Pool_mk1;
 
 public class Application 
 {
 
 	public static void main(String[] args) 
 	{
+		var s = new Scanner(System.in);
+		
+		System.out.println("Digite o tamanho do array : ");
+		int tamanho = s.nextInt();
+		
+		System.out.println("Digite o id do array: ");
+		int id = s.nextInt();
 		
 		// perguntar o tamanho do array
 		// perguntar o id do array
 		
-		Pool_mk1 pool = new Pool_mk1();
+
 	}
 
 }
