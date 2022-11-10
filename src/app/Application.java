4659e24c9ce4908ca3e8329ad8596c8edcb66afa
Marco Aurélio De Faria <marcoaureliodefariar@gmail.com>
2022-11-10 11:38:39 -0300

    top

--- src/app/Application.java
+++ src/app/Application.java
@@ -7,23 +7,17 @@ public class Application
 
 	public static void main(String[] args) 
 	{
-		
-	/*	
-		Scanner s = new Scanner(System.in);
-		// perguntar o tamanho do array
-		System.out.println("Digite o tamanho do array : ");
-		int tamanho = s.nextInt();
-		// perguntar o id do array
-		System.out.println("Digite o id do array: ");
-		int id = s.nextInt();
-		System.out.println("Digite o o tanto de pessoas: ");
-		Pessoa_Thread[] pessoas = new Pessoa_Thread[s.nextInt()];
-
-		
-		System.out.println(tamanho+ id );
-		
-		Pool_mk2 pool = new Pool_mk2(id, tamanho , new Front(), pessoas);
-		*/
+		int autura, largura, portas , pessoas;
+		Scanner ip = new Scanner(System.in);
+		System.out.println("incira a largura");
+		largura = ip.nextInt();
+		System.out.println("incira a autura");
+		autura = ip.nextInt();
+		System.out.println("incira a portas");
+		portas = ip.nextInt();
+		System.out.println("incira a pessoas");
+		pessoas = ip.nextInt();
+	
 		
 		
 	}
